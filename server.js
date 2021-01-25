dd533d629c7ba0ec4880e52a4b2110c322a1f289
sumukhg <gawsh4@gmail.com>
2021-01-25 21:43:54 +0530

    Changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path=require('path');
 
 const app=express();
 
-app.use(express.static(__dirname +'/dist/BarclaysHackathonAngular'));
+app.use(express.static(__dirname +'/dist/my-book-store'));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname+'dist/BarclaysHackathonAngular/index.html'));
+    res.sendFile(path.join(__dirname+'dist/my-book-store/index.html'));
   });
 
   app.listen(process.env.PORT || 8080);
